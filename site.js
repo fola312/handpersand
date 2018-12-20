88c26bf15e9911cf4a9744b5218f99b785ee7d3f
Fola Orokunle <robingoodfellow17@users.noreply.github.com>
2018-12-19 21:42:58 -0500

    1st run, 2 col

---
+++ site.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "400px";
+    document.getElementById("container").style.marginLeft = "400px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("container").style.marginLeft= "0";
+}
